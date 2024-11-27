d0eef904cb07838db52e979e6e9df5ff602008a9
kotobuki daqua <kotobuki@daqua.jp>
2024-11-27 15:00:49 +0900

    タスクトレイアイコンにアプリを収納できるかを試すプロジェクト しかしプラグインが無い的なエラーメッセージが出るので調査開始 ドキュメント上の同カテゴリ（JavascriptAPI)にある本体バージョン等取得は成功することを確認

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
+// import {tray} from "@tauri-apps/api";
+import {getVersion, getTauriVersion, getName} from '@tauri-apps/api/app';
 
-createApp(App).mount("#app");
+const main = async () => {
+    const appVersion = await getVersion();
+    const tauriVersion = await getTauriVersion();
+    const name = await getName();
+    console.log({
+        appVersion,
+        tauriVersion,
+        name
+    });
+
+    // const _tray = await tray.TrayIcon.new({
+    //     tooltip: 'new tooltip1',
+    // });
+    // console.log(_tray)
+
+    createApp(App).mount("#app");
+};
+await main();
+
