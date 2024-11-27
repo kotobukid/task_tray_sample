ebf3cdbf40849de22eda882e515063a0794545e7
kotobuki daqua <kotobuki@daqua.jp>
2024-11-27 16:27:47 +0900

    型の理解が深まる

--- src/composables/image.ts
+++ src/composables/image.ts
@@ -2,11 +2,11 @@ import {image} from "@tauri-apps/api";
 
 const useImage = () => {
     const parse = async (content: image.Image): Promise<ImageData> => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async (resolve, reject): Promise<void> => {
 
             if (content.rid) { // Check if content has rid property
-                const buffer = await content.rgba();
-                const size = await content.size();
+                const buffer: Uint8Array = await content.rgba();
+                const size: image.ImageSize = await content.size();
                 resolve(new ImageData(
                     new Uint8ClampedArray(buffer),
                     size.width,
